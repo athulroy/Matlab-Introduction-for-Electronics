6a5d664eb9c64fd473c6df98b82cbbffbbe7c340
Nikhil Francis Giji <45326576+nikhilgiji@users.noreply.github.com>
2019-08-20 12:35:33 +0530

    Update unitimpulsesignal.m

--- unitimpulsesignal.m
+++ unitimpulsesignal.m
@@ -1,23 +1,6 @@
 clc 
 clear all
 close all
-n=[-5:1:5]
-q=length(n);
-j=1;
-for i=1:q
-    if n(1)<0
-        amp(j)=0;
-        j=j+1;
-    else
-        amp(j)=1;
-        j=j+1;
-    end
-end
-subplot(2,1,1);
-stem(n,amp)
-xlabel("time")
-ylabel("amplitude")
-title("unit step signal")
 %program for unit impulse signal
 n=[-5:1:5]
 q=length(n)
